d979897b71fe89f3abe8fd27881faa746ef0ab7a
Ashley Dittmer <ashleydittmer@Ashleys-MacBook-Pro-2.local>
2026-07-30 10:34:36 -0400

    Ading soome ci code

--- playwright.config.ts
+++ playwright.config.ts
@@ -57,7 +57,7 @@ export default defineConfig({
      {
        name: 'Mobile Safari',
        use: { ...devices['iPhone 12'] },
-     },
+     }, 
 
     /* Test against branded browsers. */
      {
